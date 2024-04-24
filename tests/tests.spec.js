7171b89b3920230e9bb678ade0a2a6ba37cf292e
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2024-04-24 11:21:06 +0200

    test easier to read and fix mistake

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -10,19 +10,15 @@ test('Test page nagavation', async ({ page }) => {
   await expect(page.getByRole('heading')).toHaveText('Login')
 
   await page.getByRole('button', { name: 'Login' }).click()
-
   await expect(page.getByRole('heading')).toHaveText('Home Page')
 
   await page.locator('.nav-link', {hasText: 'Privacy'}).click()
-
   await expect(page.getByRole('heading').nth(0)).toHaveText('Privacy Policy')
 
   await page.locator('.nav-link', {hasText: 'Home'}).click()
-
   await expect(page.getByRole('heading').nth(0)).toHaveText('Home Page')
 
   await page.locator('.nav-link', {hasText: 'Logout'}).click()
-
   await expect(page.getByRole('heading').nth(0)).toHaveText('Login')
 
 });
@@ -30,20 +26,17 @@ test('Test page nagavation', async ({ page }) => {
 test('Test test creation', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Login' }).click()
-  
   await page.locator('.icon-link', {hasText: 'Create new quiz'}).click() 
   
   await expect(page.getByRole('heading').nth(0)).toHaveText('Create New Quiz')
 
-  await page.getByRole('button', { name: 'Add Another Question' })
-
   await expect( page.getByText('Question 1')).toBeVisible()
 
-  await page.getByRole('button', { name: 'Add Another Question' })
+  await page.getByRole('button', { name: 'Add Another Question' }).click()
 
   await expect( page.getByText('Question 2')).toBeVisible()
 
-  await page.getByRole('button', { name: 'Add Another Question' })
+  await page.getByRole('button', { name: 'Add Another Question' }).click()
 
   await expect( page.getByText('Question 3')).toBeVisible()
 });
