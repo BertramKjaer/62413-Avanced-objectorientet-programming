60ac43a74edaa2d41e1865ce33a168e7be4a0a75
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2024-05-01 22:53:14 +0200

    Update tests to match new code

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -1,4 +1,3 @@
-// @ts-check
 const { test, expect } = require('@playwright/test')
 
 test.beforeEach('', async ({ page }) => {
@@ -7,34 +6,54 @@ test.beforeEach('', async ({ page }) => {
 
 test('Test: page nagavation', async ({ page }) => {
 
-  await expect(page.getByRole('heading')).toHaveText('Login')
+  await page.getByLabel('Username').fill('s215779@dtu.dk');
+  await page.getByLabel('Password').fill('Kv08-650b');
+  await page.getByRole('button', { name: 'Login' }).click();
 
-  await page.getByRole('button', { name: 'Login' }).click()
-  await expect(page.getByRole('heading')).toHaveText('Home Page')
-
-  await page.locator('.nav-link', {hasText: 'Privacy'}).click()
-  await expect(page.getByRole('heading').nth(0)).toHaveText('Privacy Policy')
-
-  await page.locator('.nav-link', {hasText: 'Home'}).click()
-  await expect(page.getByRole('heading').nth(0)).toHaveText('Home Page')
-
-  await page.locator('.nav-link', {hasText: 'Logout'}).click()
-  await expect(page.getByRole('heading').nth(0)).toHaveText('Login')
+  await page.getByRole('link', { name: 'Privacy' }).click();
+  await page.getByRole('link', { name: 'Home' }).click();
+  await page.getByRole('link', { name: ' View Quizzes' }).click();
+  await page.getByRole('link', { name: '' }).click();
+  await page.getByRole('link', { name: ' Create new quiz' }).click();
+  await expect(page.getByRole('heading', { name: 'Create New Quiz' })).toBeVisible()
 
 });
 
 test('Test: test creation', async ({ page }) => {
 
-  await page.getByRole('button', { name: 'Login' }).click()
-  await page.locator('.icon-link', {hasText: 'Create new quiz'}).click() 
-  
-  await expect(page.getByRole('heading').nth(0)).toHaveText('Create New Quiz')
-  await expect( page.getByText('Question 1')).toBeVisible()
+  await page.getByLabel('Username').fill('s215779@dtu.dk');
+  await page.getByLabel('Password').fill('Kv08-650b');
+  await page.getByRole('button', { name: 'Login' }).click();
 
-  await page.getByRole('button', { name: 'Add Another Question' }).click()
-  await expect( page.getByText('Question 2')).toBeVisible()
+  await page.getByRole('link', { name: ' Create new quiz' }).click();
 
-  await page.getByRole('button', { name: 'Add Another Question' }).click()
-  await expect( page.getByText('Question 3')).toBeVisible()
+  await page.getByLabel('Course ID').fill('62413');
+
+  await page.getByLabel('Question').click();
+  await page.getByLabel('Question').fill('1');
+  await page.getByLabel('Correct Answer', { exact: true }).click();
+  await page.getByLabel('Correct Answer', { exact: true }).fill('2');
+  await page.getByLabel('Incorrect Answer 1').fill('3');
+  await page.getByLabel('Incorrect Answer 1').click();
+  await page.getByLabel('Incorrect Answer 2').click();
+  await page.getByLabel('Incorrect Answer 2').fill('4');
+  await page.getByLabel('Incorrect Answer 3').click();
+  await page.getByLabel('Incorrect Answer 3').fill('5');
+
+  await page.getByRole('button', { name: 'Add Another Question' }).click();
+
+  await page.locator('input[name="Input\\.Questions\\[1\\]\\.QuizQuestion"]').click();
+  await page.locator('input[name="Input\\.Questions\\[1\\]\\.QuizQuestion"]').fill('6');
+  await page.locator('input[name="Input\\.Questions\\[1\\]\\.CorrectAnswer"]').click();
+  await page.locator('input[name="Input\\.Questions\\[1\\]\\.CorrectAnswer"]').fill('7');
+  await page.locator('input[name="Input\\.Questions\\[1\\]\\.IncorrectAnswer1"]').click();
+  await page.locator('input[name="Input\\.Questions\\[1\\]\\.IncorrectAnswer1"]').fill('8');
+  await page.locator('input[name="Input\\.Questions\\[1\\]\\.IncorrectAnswer2"]').click();
+  await page.locator('input[name="Input\\.Questions\\[1\\]\\.IncorrectAnswer2"]').fill('9');
+  await page.locator('input[name="Input\\.Questions\\[1\\]\\.IncorrectAnswer3"]').click();
+  await page.locator('input[name="Input\\.Questions\\[1\\]\\.IncorrectAnswer3"]').fill('0');
+
+  await page.getByRole('button', { name: 'Create Quiz' }).click();
+
 });
 
