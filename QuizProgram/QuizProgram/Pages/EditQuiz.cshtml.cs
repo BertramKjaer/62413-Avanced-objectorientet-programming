bceb3fc9be8696f5501712ba4e6ff064ae1491bf
Magnusbruus <magnusbruus@gmail.com>
2024-05-01 15:13:12 +0200

    Working editQuiz with bug that removes all questions but one when editing

--- QuizProgram/QuizProgram/Pages/EditQuiz.cshtml.cs
+++ QuizProgram/QuizProgram/Pages/EditQuiz.cshtml.cs
@@ -52,7 +52,7 @@ namespace QuizProgram.Pages
         {
             if (!ModelState.IsValid)
             {
-                return Page(); // Return if there are validation errors
+                return Page(); // Validation failed, re-display the form
             }
 
             var quizToUpdate = await _context.Quizzes
@@ -64,44 +64,46 @@ namespace QuizProgram.Pages
                 return NotFound($"Unable to load quiz with ID '{id}'.");
             }
 
-            quizToUpdate.Title = Input.Title; // Update the title
+            // Update quiz title
+            quizToUpdate.Title = Input.Title;
 
-            // Existing question IDs from the database
-            var existingQuestionIds = quizToUpdate.Questions.Select(q => q.QuestionId).ToList();
-            // IDs from the input model
+            // List of question IDs from the form input
             var inputQuestionIds = Input.Questions.Select(q => q.QuestionId).ToList();
 
-            // Removing questions that are not present in the input model
+            // Identify questions to be removed
             var questionsToRemove = quizToUpdate.Questions
-                .Where(q => !inputQuestionIds.Contains(q.QuestionId)).ToList();
+                .Where(q => !inputQuestionIds.Contains(q.QuestionId))
+                .ToList();
+
+            // Remove questions directly from quizToUpdate
             foreach (var question in questionsToRemove)
             {
-                _context.Questions.Remove(question);
+                quizToUpdate.Questions.Remove(question);
             }
 
-            // Update or add questions
-            foreach (var questionModel in Input.Questions)
+            // Update existing questions and add new ones
+            foreach (var inputQuestion in Input.Questions)
             {
-                var question = quizToUpdate.Questions.FirstOrDefault(q => q.QuestionId == questionModel.QuestionId);
+                var question = quizToUpdate.Questions.FirstOrDefault(q => q.QuestionId == inputQuestion.QuestionId);
                 if (question != null)
                 {
                     // Update existing question
-                    question.QuizQuestion = questionModel.QuizQuestion;
-                    question.CorrectAnswer = questionModel.CorrectAnswer;
-                    question.IncorrectAnswer1 = questionModel.IncorrectAnswer1;
-                    question.IncorrectAnswer2 = questionModel.IncorrectAnswer2;
-                    question.IncorrectAnswer3 = questionModel.IncorrectAnswer3;
+                    question.QuizQuestion = inputQuestion.QuizQuestion;
+                    question.CorrectAnswer = inputQuestion.CorrectAnswer;
+                    question.IncorrectAnswer1 = inputQuestion.IncorrectAnswer1;
+                    question.IncorrectAnswer2 = inputQuestion.IncorrectAnswer2;
+                    question.IncorrectAnswer3 = inputQuestion.IncorrectAnswer3;
                 }
                 else
                 {
-                    // Add new question if it doesn't exist
+                    // Add new question
                     quizToUpdate.Questions.Add(new Question
                     {
-                        QuizQuestion = questionModel.QuizQuestion,
-                        CorrectAnswer = questionModel.CorrectAnswer,
-                        IncorrectAnswer1 = questionModel.IncorrectAnswer1,
-                        IncorrectAnswer2 = questionModel.IncorrectAnswer2,
-                        IncorrectAnswer3 = questionModel.IncorrectAnswer3
+                        QuizQuestion = inputQuestion.QuizQuestion,
+                        CorrectAnswer = inputQuestion.CorrectAnswer,
+                        IncorrectAnswer1 = inputQuestion.IncorrectAnswer1,
+                        IncorrectAnswer2 = inputQuestion.IncorrectAnswer2,
+                        IncorrectAnswer3 = inputQuestion.IncorrectAnswer3
                     });
                 }
             }
@@ -109,11 +111,11 @@ namespace QuizProgram.Pages
             try
             {
                 await _context.SaveChangesAsync();
-                return RedirectToPage("./ViewQuiz"); // Make sure this is the correct redirect
+                return RedirectToPage("./ViewQuiz"); // Redirect after successful update
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!_context.Quizzes.Any(q => q.QuizId == id))
+                if (!QuizExists(id))
                 {
                     return NotFound();
                 }
@@ -123,6 +125,18 @@ namespace QuizProgram.Pages
                 }
             }
         }
+
+
+
+
+
+
+        private bool QuizExists(int id)
+        {
+            return _context.Quizzes.Any(e => e.QuizId == id);
+        }
+
+
 
 
     }
